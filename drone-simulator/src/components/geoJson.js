d90c75f70df81ca3eaaad81f03b32d6176026ede
Prashant Chavan <prashantch451@outlook.com>
2023-02-07 23:24:29 +0530

    instead of the automatic path drone is following path provided in the geolocation file co-ordinates

--- drone-simulator/src/components/geoJson.js
+++ drone-simulator/src/components/geoJson.js
@@ -1,18 +1,54 @@
-const geojson = {
-    type: 'FeatureCollection',
-    features: [
-        {
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                coordinates: [73.85663772073566, 18.520064783835046]
-            },
-            properties: {
-                title: 'Mapbox',
-                description: 'Washington, D.C.'
-            }
-        },
-    ]
-};
+const geojson = [
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52011
+    },
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52012
+    },
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52013
+    },
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52014
+    },
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52015
+    },
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52016
+    },
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52017
+    },
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52018
+    },
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52019
+    },
+    {
+        "time": 0,
+        "lat": 73.85660,
+        "lng": 18.52020
+    }
+]
 
 export default geojson;

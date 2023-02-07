6accbb4bbb47686b97c6d01f92afc156c234474a
Prashant Chavan <prashantch451@outlook.com>
2023-02-07 18:57:36 +0530

    indentation fixed and dependencies are imported

--- drone-simulator/src/components/MapComponent.jsx
+++ drone-simulator/src/components/MapComponent.jsx
@@ -1,16 +1,20 @@
-import * as React from 'react';
-import Map from 'react-map-gl';
+import mapboxgl from "!mapbox-gl";
+import React, { useRef, useEffect, useState } from "react";
 
 function MapComponent() {
-  return <Map
-    initialViewState={{
-      longitude: 73.85680938211894,
-      latitude:  18.520227555214063,
-      zoom: 10
-    }}
-    mapboxAccessToken={"pk.eyJ1IjoicHJhc2hhbnRjaHZuIiwiYSI6ImNsZHUzdnJoeTAyMmgzeHJmZDl4cDVkb2MifQ.JBUOJpgp3v-dAdXWpYCNXg"}
-    style={{width: 2080, height: 1080}}
-    mapStyle="mapbox://styles/mapbox/streets-v9"
-  />;
+  return (
+    <Map
+      initialViewState={{
+        longitude: 73.85680938211894,
+        latitude: 18.520227555214063,
+        zoom: 10,
+      }}
+      mapboxAccessToken={
+        "pk.eyJ1IjoicHJhc2hhbnRjaHZuIiwiYSI6ImNsZHUzdnJoeTAyMmgzeHJmZDl4cDVkb2MifQ.JBUOJpgp3v-dAdXWpYCNXg"
+      }
+      style={{ width: 2080, height: 1080 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
+  );
 }
 export default MapComponent;

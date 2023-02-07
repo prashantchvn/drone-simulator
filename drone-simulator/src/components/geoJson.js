e2ab0801de62cbc9d968f00d2e7412bd0d986d47
Prashant Chavan <prashantch451@outlook.com>
2023-02-07 22:42:19 +0530

    styling and the icon of location moved from marker to drone

---
+++ drone-simulator/src/components/geoJson.js
@@ -0,0 +1,18 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [73.85663772073566, 18.520064783835046]
+            },
+            properties: {
+                title: 'Mapbox',
+                description: 'Washington, D.C.'
+            }
+        },
+    ]
+};
+
+export default geojson;

6b3b5043f5c04f41628d9e4e17d7525675cdddfb
Prashant Chavan <prashantch451@outlook.com>
2023-02-07 19:32:32 +0530

    marker is added in the map container

--- drone-simulator/src/components/MapComponent.jsx
+++ drone-simulator/src/components/MapComponent.jsx
@@ -4,22 +4,29 @@ import React, { useRef, useEffect, useState } from "react";
 function MapComponent() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
+  const [lng, setLng] = useState(73.85663772073566);
+  const [lat, setLat] = useState(18.520064783835046);
   const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
+    let mapInstance = map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/prashantchvn/cldu952og000z01mzj256jcip",
       center: [lng, lat],
       zoom: zoom,
+    }).on("load",() => {
+      const el = document.createElement("div");
+      el.className = "marker";
+      new mapboxgl.Marker(el)
+        .setLngLat([73.85663772073566, 18.520064783835046])
+        .addTo(mapInstance);
     });
   });
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
+
     map.current.on("move", () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
@@ -27,7 +34,12 @@ function MapComponent() {
     });
   });
 
-  mapboxgl.accessToken = "pk.eyJ1IjoicHJhc2hhbnRjaHZuIiwiYSI6ImNsZHUzdnJoeTAyMmgzeHJmZDl4cDVkb2MifQ.JBUOJpgp3v-dAdXWpYCNXg";
+  useEffect(() => {
+    if (!map.current) return;
+  }, []);
+
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoicHJhc2hhbnRjaHZuIiwiYSI6ImNsZHUzdnJoeTAyMmgzeHJmZDl4cDVkb2MifQ.JBUOJpgp3v-dAdXWpYCNXg";
 
   return (
     <div>

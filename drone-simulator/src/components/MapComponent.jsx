7658cef719f784cc939ba1db157cb2de22793f3a
Prashant Chavan <prashantch451@outlook.com>
2023-02-09 02:51:47 +0530

    data is loaded into map

--- drone-simulator/src/components/MapComponent.jsx
+++ drone-simulator/src/components/MapComponent.jsx
@@ -1,6 +1,6 @@
 import mapboxgl from "mapbox-gl";
 import React, { useRef, useEffect, useState } from "react";
-import geojson from "./geoJson";
+import axios from "axios";
 import pause from "../icons/pause.png";
 import play from "../icons/play.png";
 import { toast } from "react-toastify";
@@ -8,9 +8,10 @@ import { toast } from "react-toastify";
 function MapComponent() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(geojson[0].lat);
-  const [lat, setLat] = useState(geojson[0].lng);
+  const [lng, setLng] = useState();
+  const [lat, setLat] = useState();
   const [zoom, setZoom] = useState(18.5);
+  const [geojson, setGeojson] = useState([]);
   //marker for the map
   let marker;
   let mapInstance;
@@ -19,31 +20,51 @@ function MapComponent() {
   let i = 1;
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
-    mapInstance = map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/prashantchvn/cldu952og000z01mzj256jcip",
-      center: [lng, lat],
-      zoom: zoom,
-    }).on("load", () => {
-      const el = document.createElement("div");
-      el.className = "marker";
-      marker = new mapboxgl.Marker(el).setLngLat([lat, lng]).addTo(mapInstance);
-    });
-  });
+    axios
+      .get("http://localhost:5000/api/waypoints")
+      .then((res) => {
+        setGeojson(res.data.data);
+        setLat(res.data.data[0].lat);
+        setLng(res.data.data[0].lng);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Something went wrong");
+      });
+  }, []);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (isPlaying) {
-        if (i >= geojson.length){
-          isPlaying = false;
-          toast("Drone reached destination")
-        }; // breaking condition
-        marker.setLngLat([geojson[i].lat, geojson[i].lng]);
-        i++;
-      }
-    }, 2000);
-    return () => clearInterval(interval);
+    if (geojson.length) {
+      if (map.current) return; // initialize map only once
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/prashantchvn/cldu952og000z01mzj256jcip",
+        center: [geojson[0].lat, geojson[0].lng],
+        zoom: zoom,
+      }).on("load", () => {
+        const el = document.createElement("div");
+        el.className = "marker";
+        marker = new mapboxgl.Marker(el)
+          .setLngLat([geojson[0].lat, geojson[0].lng])
+          .addTo(mapInstance);
+      });
+    }
+  },[geojson]);
+
+  useEffect(() => {
+    if (geojson.length) {
+      const interval = setInterval(() => {
+        if (isPlaying) {
+          if (i >= geojson.length) {
+            isPlaying = false;
+            toast("Drone reached destination");
+          } // breaking condition
+          marker.setLngLat([geojson[i].lat, geojson[i].lng]);
+          i++;
+        }
+      }, 2000);
+      return () => clearInterval(interval);
+    }
   }, []);
 
   useEffect(() => {
@@ -59,33 +80,37 @@ function MapComponent() {
   mapboxgl.accessToken =
     "pk.eyJ1IjoicHJhc2hhbnRjaHZuIiwiYSI6ImNsZHUzdnJoeTAyMmgzeHJmZDl4cDVkb2MifQ.JBUOJpgp3v-dAdXWpYCNXg";
 
-  return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+  if (geojson.length) {
+    return (
+      <div>
+        <div className="sidebar">
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+        <div className="sidebar-right">
+          <img
+            src={play}
+            onClick={() => {
+              if (!isPlaying) {
+                isPlaying = true;
+                toast("Animation Resumed");
+              }
+            }}
+          />
+          <img
+            src={pause}
+            onClick={() => {
+              if (isPlaying) {
+                isPlaying = false;
+                toast("Animation Paused");
+              }
+            }}
+          />
+        </div>
+        <div ref={mapContainer} className="map-container" />
       </div>
-      <div className="sidebar-right">
-        <img
-          src={play}
-          onClick={() => {
-            if(!isPlaying){
-              isPlaying = true;
-              toast("Animation Resumed");  
-            }
-          }}
-        />
-        <img
-          src={pause}
-          onClick={() => {
-            if (isPlaying) {
-              isPlaying = false;
-              toast("Animation Paused");
-            }
-          }}
-        />
-      </div>
-      <div ref={mapContainer} className="map-container" />
-    </div>
-  );
+    );
+  } else {
+    return <h3>loading ...</h3>;
+  }
 }
 export default MapComponent;
